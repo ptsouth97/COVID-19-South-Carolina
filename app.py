21f4ec3f788c84c053849bb32cb0ac17fee813ce
ptsouth97 <ptsouth97@gmail.com>
2020-05-31 09:15:02 -0400

    cleaned up chart format

--- app.py
+++ app.py
@@ -25,34 +25,34 @@ def main():
 	fig.set_size_inches(11, 8.5)
 
 	# Plot data
-	ax = df['New Cases (SC)'].plot(kind='line', color='k', marker='.', linewidth=1, logy=True, legend=True)
-	ax = df['Daily deaths'].plot(kind='line', color='r', marker='x', linewidth=1, logy=True, legend=True)
+	ax1 = df['New Cases (SC)'].plot(kind='line', color='k', marker='.', linewidth=1, logy=True, legend=True)
+	ax1 = df['Daily deaths'].plot(kind='line', color='r', marker='x', linewidth=1, logy=True, legend=True)
 
 	# Set the range for the y-axis
-	ax.set_ylim([1, 1000000])
+	ax1.set_ylim([1, 1000000])
 
 	# Turn on major and minor grid lines for y-axis
-	ax.yaxis.grid(True, color='lightsteelblue', which='minor', linestyle='-', alpha=0.5)
-	ax.yaxis.grid(True, color='lightsteelblue', which='major', linestyle='-', alpha=0.8)
+	ax1.yaxis.grid(True, color='lightsteelblue', which='minor', linestyle='-', alpha=0.5)
+	ax1.yaxis.grid(True, color='lightsteelblue', which='major', linestyle='-', alpha=0.8)
 
 	# Set the range for the x-axis
-	#ax.set_xlim([datetime.date(2020, 3, 1), datetime.date(2020, 7, 19)])
-	ax.set_xlim([0, 140])
+	#ax1.set_xlim([datetime.date(2020, 3, 1), datetime.date(2020, 7, 19)])
+	ax1.set_xlim([0, 140])
 
 	# Set x-labels
-	#ax.set_xticklabels(np.arange(0, 141, 7))
+	#ax1.set_xticklabels(np.arange(0, 141, 7))
 	
 
 	# Set the location for x-axis ticks
-	ax.xaxis.set_minor_locator(MultipleLocator(1))
-	ax.xaxis.set_major_locator(MultipleLocator(7))
+	ax1.xaxis.set_minor_locator(MultipleLocator(1))
+	ax1.xaxis.set_major_locator(MultipleLocator(7))
 
 	# Turn on the major and minor grid lines for x-axis
-	ax.xaxis.grid(True, color='lightsteelblue', which='minor', linestyle='-', alpha=0.5)
-	ax.xaxis.grid(True, color='lightsteelblue', which='major', linestyle='-', alpha=0.8)
+	ax1.xaxis.grid(True, color='lightsteelblue', which='minor', linestyle='-', alpha=0.5)
+	ax1.xaxis.grid(True, color='lightsteelblue', which='major', linestyle='-', alpha=0.8)
 
 	# Create secondary x-axis
-	ax2 = ax.twiny()
+	ax2 = ax1.twiny()
 
 	newlabel = list(df['Date'])
 	newlabel = newlabel[::28]
@@ -61,8 +61,11 @@ def main():
 
 	# Label axes and title
 	plt.title('2019 nCoV Daily Cases in South Carolina', color='midnightblue')
-	plt.xlabel('Successive Calendar Days')
-	plt.ylabel('Counts of Cases and Deaths')
+
+	ax1.set_xlabel('Successive Calendar Days')
+	ax1.set_ylabel('Counts of Cases and Deaths')
+
+	plt.tight_layout()
 
 	plt.savefig('SC-chart.png')
 	plt.show()
