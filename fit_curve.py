32c214ad1e5242c42f96a787f033061479d6d6cf
ptsouth97 <ptsouth97@gmail.com>
2020-06-05 12:22:36 -0400

    updated regression with multiple trends

--- fit_curve.py
+++ fit_curve.py
@@ -29,134 +29,57 @@ def main():
 
 def regression(df, date, multi_trend):
 	''' performs linear regression'''
-	'''
+
+	# Initialize blank data frame
+	predictions = pd.DataFrame()
+
 	while True:
 	
-		if multi_trend == True:
-			trends = int(input('How many trends? '))
+		# Get the date range of the trend to be analyzed
+		start_day = int(input('What is the start day of the trend? '))
+		end_day = int(input('What is the end day of the trend? '))
+		celeration_days = end_day - start_day
+		shift = start_day
 
-		else:
-			trends = 1
+		# Slice the dataframe based on those dates
+		current_df = df.iloc[start_day:end_day+1, :]
 
-		for trend in range(0, trends):
+		current_df = current_df.fillna(1)
+		current_df = current_df.replace(to_replace=0, value=1)
+		
+		# Create X and Y prediction spaces
+		num = len(current_df)
+		X = np.arange(0, num).reshape(-1, 1)
+		Y = current_df.iloc[0:num, 1].values.reshape(-1, 1)
+		
+		# Perform regression	
+		linear_regressor = LinearRegression()
+		linear_regressor.fit(X, np.log(Y))
 
-			# find number of days between end of chart and start date
-			if multi_trend == False:
-				start = df.index[0]
-				end = datetime.datetime(2020, 5, 17)
+		# Get linear regression parameters
+		m = linear_regressor.coef_
+		b = linear_regressor.intercept_
 
-			else:
-				start_year = int(input('What is the start date year? '))
-				start_mon = int(input('What is the start date month? '))
-				start_day = int(input('What is the start date day? '))
-				start = datetime.datetime(start_year, start_mon, start_day)
+		values = []
 
-				end_year = int(input('What is the end date year? '))
-				end_mon = int(input('What is the end date month? '))
-				end_day = int(input('What is the end date day? '))
-				end = datetime.datetime(end_year, end_mon, end_day)
+		for value in range(start_day, end_day+1):
+			values.append(math.exp(m*(value-shift) + b))
 
-			df = df.loc[start:end]
-			
-			print(df)
+		values = pd.DataFrame(values, index=np.arange(start_day,(end_day+1)))
+		#print(values)	
 
-			days = (end - start).days
-			shift = 140 - days
-	'''
+		predictions = predictions.append(values, ignore_index=True)
 
-	# Get the date range of the trend to be analyzed
-	start_day = 13
-	end_day = 32
-	celeration_days = end_day - start_day
-	shift = start_day
+		more_trends = input('Do you want to add more trends? ')
 
-	# Slice the dataframe based on those dates
-	df = df.iloc[start_day:end_day+1, :]
-
-	df = df.fillna(1)
-	df = df.replace(to_replace=0, value=1)
-
-	# Create X and Y prediction spaces
-	num = len(df)
-	X = np.arange(0, num).reshape(-1, 1)
-	Y = df.iloc[0:num, 1].values.reshape(-1, 1)
-
-	# Perform regression	
-	linear_regressor = LinearRegression()
-	linear_regressor.fit(X, np.log(Y))
-
-	# Get linear regression parameters
-	m = linear_regressor.coef_
-	b = linear_regressor.intercept_
-
-	values = []
-
-	for value in range(start_day, end_day+1):
-		values.append(math.exp(m*(value-shift) + b))
-
-	values = pd.DataFrame(values, index=np.arange(start_day,(end_day+1)))
-	#print(values)	
-
+		if more_trends != 'Yes':
+			break
 
 	# Create a data frame the size of the celeration chart and fill it with the calculated values
 	#predictions = pd.DataFrame(index=np.arange(0, 141), data=None)
 	#print(predictions)
 	
-
-
-	'''
-			# Initialize and fill data frame for regression results
-			values = []
-	
-			for value in range(1, days): #140): #days+1):   # changed 'begin' from '1'
-				values.append(math.exp(m*(value-shift) + b))
-
-			# Create a data frame the size of the celeration chart and fill it with the calculated values
-			predictions = pd.DataFrame(index=[np.arange(1, days)], data=values)
-
-			# Create a data frame to join with the predictions that matches the size of the celeration chart
-			total_days = 140
-			early_days = 140 - days
-			early_predictions = pd.DataFrame(np.zeros((early_days, 1)))
-
-			predictions = predictions.append(early_predictions)
-			predictions.reset_index(inplace=True)
-			predictions.drop(['index'], axis=1, inplace=True)
-
-
-			# Convert index back to dates
-			dates = predictions.index.values
-			dates = dates.tolist()
-	
-			dates = [ datetime.datetime(2019, 12, 29) + datetime.timedelta(days=date) for date in dates ]
-
-			predictions['dates'] = dates
-			predictions.set_index('dates', inplace=True)
-			predictions = predictions.rename(columns={0: 'celeration curve'})
-			predictions.replace(to_replace=0, value=np.nan, inplace=True)
-	
-
-			# Calculate the celeration value
-			week1 = float(predictions.iloc[78])
-			week0 = float(predictions.iloc[71])
-			celeration = week1 / week0
-
-			original_df = pd.concat([df, predictions], axis=1)
-
-			if multi_trend == True:
-				print(original_df)
-				charting.plot_data(original_df, 'China', None, celeration, date, 'solo', multi_trend)
-		
-		if multi_trend == True:
-			satisfied = input("Are you satisfied with the result? ")
-				
-			if satisfied == "Yes":
-				break
-
-		else:
-			break
-	'''
-	return values #y_pred  #original_df, celeration
+	return predictions #y_pred  #original_df, celeration
 
 
 if __name__ == '__main__':
